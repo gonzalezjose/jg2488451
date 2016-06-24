a7cd7368964a725b5ac4b4b53864b10e4c9da173
gonzalezjose <jgonzalezh@student.rcc.edu>
2016-06-24 15:50:14 -0700

    HelloWorld

---
+++ Hmwk/HelloWorld/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose A. Gonzalez *
+ * Created on June 23, 2016, 7:04 PM
+ * Purpose: Hello World
+ */
+
+//System Libraries
+#include <iostream>         //Input/Output Library
+using namespace std;        //Namespaceof the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    std::cout<<"Hello World"<<std::endl;    
+    
+    //Exit Stage Right!
+
+    return 0;
+}
+
---
+++ Hmwk/Template/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose A. Gonzalez *
+ * Created on June 23, 2016, 7:04 PM
+ * Purpose: Template
+ */
+
+//System Libraries
+#include <iostream>         //Input/Output Library
+using namespace std;        //Namespaceof the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Exit Stage Right!
+
+    return 0;
+}
+
